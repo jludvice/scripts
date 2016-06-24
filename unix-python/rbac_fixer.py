5b28a1808c991863a19cb0c0598af8fe1a5ccd82
Josef Ludvicek <josef.ludvicek.cz@gmail.com>
2016-06-24 11:35:31 +0200

    ignore commented lines in rbac fixer

--- unix-python/rbac_fixer.py
+++ unix-python/rbac_fixer.py
@@ -13,7 +13,7 @@ rel_paths_fabric = (
     'fabric/fabric8-karaf/src/main/resources/etc/jmx.acl.*',
     'fabric/fabric8-karaf/src/main/resources/distro/fabric/import/fabric/profiles/acls.profile/jmx.acl.*',
     'fabric/fabric8-karaf/src/main/resources/distro/fabric/import/fabric/profiles/insight/elasticsearch.node.profile/jmx.acl.*',
-    'tooling/rh-support/support-profiles/src/main/resources/fabric/import/fabric/profiles/support/base.profile/jmx.acl.*'
+    'tooling/rh-support/support-profiles/src/main/resources/fabric/import/fabric/profiles/support/base.profile/jmx.acl.*',
 )
 
 rel_paths_fuse = (
@@ -26,6 +26,8 @@ base_path = '/path/to/git/repo/with/fabric8'
 rel_paths = rel_paths_fabric
 
 equal_roles = ('viewer', 'Monitor', 'Operator', 'Maintainer')
+# equal_roles = ('Administrator', 'SuperUser', 'admin')
+# equal_roles = ('Deployer', 'Auditor')
 
 
 def any_role_in_string(text='', roles=equal_roles):
@@ -77,7 +79,7 @@ for file_path in file_paths:
     for line in fileinput.input(file_path, inplace=True):
         # if line contains at elast one given role and not all of them
         # append missing
-        if any_role_in_string(text=line) and not all_equal_roles_in_string():
+        if not line.startswith('#') and any_role_in_string(text=line) and not all_equal_roles_in_string():
             # use write instead of print to avoid duplicate newline
             sys.stdout.write(append_if_missing(text=line))
 
